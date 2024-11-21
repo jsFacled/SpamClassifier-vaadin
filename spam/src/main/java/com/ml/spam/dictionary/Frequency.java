0b2f4c0a48fec9cdde74bf477f1330439f795ccb
faGitLab <facled@yahoo.com.ar>
2024-11-21 11:56:37 -0300

    Reemplazo de WordData por Frequency para simplificar el código

---
+++ spam/src/main/java/com/ml/spam/dictionary/Frequency.java
@@ -0,0 +1,32 @@
+package com.ml.spam.dictionary;
+
+
+/*
+Modelo simplificado para almacenar las frecuencias de spam y ham asociadas a cada palabra o símbolo.
+Clase simple para reemplazar WordData, eliminando redundancia (ya no contiene la palabra como atributo).
+ */
+public class Frequency {
+    private int spamFrequency;
+    private int hamFrequency;
+
+    public Frequency(int spamFrequency, int hamFrequency) {
+        this.spamFrequency = spamFrequency;
+        this.hamFrequency = hamFrequency;
+    }
+
+    public int getSpamFrequency() {
+        return spamFrequency;
+    }
+
+    public void incrementSpamFrequency() {
+        this.spamFrequency++;
+    }
+
+    public int getHamFrequency() {
+        return hamFrequency;
+    }
+
+    public void incrementHamFrequency() {
+        this.hamFrequency++;
+    }
+}

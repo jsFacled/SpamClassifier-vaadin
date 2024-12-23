f76979e7b331dbe4f730bc9e37c6f583cfb7185b
faGitLab <facled@yahoo.com.ar>
2024-12-23 10:21:07 -0300

    Se decide utilizar en las categorías el nombre con camelcase:SPAM_WORDS(spamWords)

--- spam/src/main/java/com/ml/spam/dictionary/models/WordCategory.java
+++ spam/src/main/java/com/ml/spam/dictionary/models/WordCategory.java
@@ -1,7 +1,18 @@
 package com.ml.spam.dictionary.models;
+
 public enum WordCategory {
-    SPAM_WORDS,    // Palabras relacionadas con spam
-    RARE_SYMBOLS,  // Símbolos raros
-    STOP_WORDS,    // Palabras irrelevantes
-    UNASSIGNED_WORDS      // Palabras nuevas NO ASIGNADAS TODAVÍA
+    SPAM_WORDS("spamWords"),          // Palabras relacionadas con spam
+    RARE_SYMBOLS("rareSymbols"),      // Símbolos raros
+    STOP_WORDS("stopWords"),          // Palabras irrelevantes
+    UNASSIGNED_WORDS("unassignedWords"); // Palabras nuevas NO ASIGNADAS TODAVÍA
+
+    private final String jsonKey;
+
+    WordCategory(String jsonKey) {
+        this.jsonKey = jsonKey;
+    }
+
+    public String getJsonKey() {
+        return jsonKey;
+    }
 }

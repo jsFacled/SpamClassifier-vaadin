74be5a8d9d52a3b95ed84aae5193f2654abc22b2
faGitLab <facled@yahoo.com.ar>
2025-04-27 11:21:14 -0300

    Se define el nombre de las columnas en el enum DatasetColumnName

---
+++ spam/src/main/java/com/ml/spam/datasetProcessor/models/DatasetColumnName.java
@@ -0,0 +1,38 @@
+package com.ml.spam.datasetProcessor.models;
+public enum DatasetColumnName {
+    // Features por palabra strongSpamWord (prefijo + palabra dinámica)
+    FREQ("freq_"),                  // Ej: freq_lexactions
+    RELATIVE_FREQ("relative_freq_"),// Ej: relative_freq_lexactions
+    WEIGHT("weight_"),              // Ej: weight_lexactions
+    POLARITY("polarity_"),          // Ej: polarity_lexactions
+
+    // Features globales (nombres fijos)
+    COUNT_MODERATE_SPAM_WORDS("count_moderateSpamWords"),
+    RATIO_MODERATE_SPAM_WORDS("ratio_moderateSpamWords"),
+    COUNT_WEAK_SPAM_WORDS("count_weakSpamWords"),
+    RATIO_WEAK_SPAM_WORDS("ratio_weakSpamWords"),
+    COUNT_HAM_INDICATORS("count_hamIndicators"),
+    RATIO_HAM_INDICATORS("ratio_hamIndicators"),
+    COUNT_NEUTRAL_BALANCED_WORDS("count_neutralBalancedWords"),
+    RATIO_NEUTRAL_BALANCED_WORDS("ratio_neutralBalancedWords"),
+    COUNT_STOP_WORDS("count_stopWords"),
+    RATIO_STOP_WORDS("ratio_stopWords"),
+    COUNT_RARE_SYMBOLS("count_rareSymbols"),
+    RATIO_RARE_SYMBOLS("ratio_rareSymbols"),
+    COUNT_UNASSIGNED_WORDS("count_unassignedWords"),
+    RATIO_UNASSIGNED_WORDS("ratio_unassignedWords"),
+    MESSAGE_LENGTH("longitud_mensaje"),
+    LEXICAL_DIVERSITY("lexical_diversity"),
+    NET_WEIGHTED_SCORE("net_weighted_score"),
+    LABEL("label");
+
+    private final String columnName;
+
+    DatasetColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public String get() {
+        return columnName;
+    }
+}

3f3847427ae4e4201553c3280958916089ef5fff
faGitLab <facled@yahoo.com.ar>
2025-06-03 08:54:21 -0300

    Se agregaron y probaron los métodos main que borran palabras de lexemas y lexemas completos con todo su contenido.

--- spam/src/main/java/com/ml/spam/dictionary/stageMain/lexemesMain/updateDeletingLexemesFromJsonListMain.java
+++ spam/src/main/java/com/ml/spam/dictionary/stageMain/lexemesMain/updateDeletingLexemesFromJsonListMain.java
@@ -8,9 +8,14 @@ import com.ml.spam.handlers.ResourcesHandler;
 public class updateDeletingLexemesFromJsonListMain {
     public static void main(String[] args) {
         String lexemeRepositoryPath = FilePathsConfig.LEXEMES_REPOSITORY_JSON_PATH;
+        String lexemeRepositoryPathTest = "static/dictionary/temporary/structured_lexemes_repository_test.json";
+
         String lexemesToDeleteJsonPath = "static/dictionary/lexemesToDelete.json";
 
         ResourcesHandler handler = new ResourcesHandler();
-        handler.updateDeletingLexemesFromJsonList(lexemeRepositoryPath, lexemesToDeleteJsonPath);
+//        handler.updateDeletingLexemesFromJsonList(lexemeRepositoryPath, lexemesToDeleteJsonPath);
+        handler.updateDeletingLexemesFromJsonList(lexemeRepositoryPathTest, lexemesToDeleteJsonPath);
+
+
     }
 }
--- spam/src/main/java/com/ml/spam/dictionary/stageMain/lexemesMain/updateDeletingWordsFromLexemesJsonListMain.java
+++ spam/src/main/java/com/ml/spam/dictionary/stageMain/lexemesMain/updateDeletingWordsFromLexemesJsonListMain.java
@@ -7,9 +7,16 @@ import com.ml.spam.handlers.ResourcesHandler;
 public class updateDeletingWordsFromLexemesJsonListMain {
     public static void main(String[] args) {
         String lexemeRepositoryPath = FilePathsConfig.LEXEMES_REPOSITORY_JSON_PATH;
+        String lexemeRepositoryPathTest = "static/dictionary/temporary/structured_lexemes_repository_test.json";
+
         String wordsToDeleteJsonPath = "static/dictionary/wordsToDeleteInLexemeFromLexemesRespository.json";
 
         ResourcesHandler handler = new ResourcesHandler();
-        handler.updateDeletingWordsFromLexemesJsonList(lexemeRepositoryPath, wordsToDeleteJsonPath);
+      //  handler.updateDeletingWordsFromLexemesJsonList(lexemeRepositoryPath, wordsToDeleteJsonPath);
+
+
+        //********** TEST ****************//
+        handler.updateDeletingWordsFromLexemesJsonList(lexemeRepositoryPathTest, wordsToDeleteJsonPath);
+
     }
 }
